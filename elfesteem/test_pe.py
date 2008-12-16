3d483d58c1296d9ddb70bb9686ba15eecd25d821
serpilliere <serpilliere@poulpi.ssi.corp>
2008-12-16 14:04:39 +0100

    fix test_pe

---
+++ elfesteem/test_pe.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+import pe
+from pe_init import PE
+import rlcompleter,readline,pdb, sys
+from pprint import pprint as pp
+readline.parse_and_bind("tab: complete")
+
+
+e_ = PE()
+mysh = "\xc3"
+s_text = e_.SHList.add_section(name = "text", addr = 0x1000, rawsize = 0x1000, data = mysh)
+e_.Opthdr.Opthdr.AddressOfEntryPoint = s_text.addr
+new_dll = [({"name":"kernel32.dll",
+             "firstthunk":s_text.addr+0x100},
+            ["CreateFileA", "SetFilePointer", "WriteFile", "CloseHandle"]
+            )
+           ,
+           ({"name":"USER32.dll",
+             "firstthunk":None},
+            ["SetDlgItemInt", "GetMenu", "HideCaret"]
+            )
+           ]
+e_.DirImport.add_dlldesc(new_dll)
+
+s_myimp = e_.SHList.add_section(name = "myimp", rawsize = 0x1000)    
+e_.DirImport.set_rva(s_myimp.addr)
+open('uu.bin', 'wb').write(str(e_))
+    
