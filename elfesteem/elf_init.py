8c6349739dea89f2e4ece3b93413c9de023bdbcf
serpilliere <devnull@localhost>
2012-10-03 12:40:47 +0200

    elf: fix empty section lookup

--- elfesteem/elf_init.py
+++ elfesteem/elf_init.py
@@ -508,6 +508,9 @@ class virt:
                 s_stop = s_max
 
             s_len = s_stop - s_start
+            if s_len == 0:
+                log.warn('empty section! %x'%start)
+                return
             total_len -= s_len
             start += s_len
             n_item = slice(s_start, s_stop, step)
