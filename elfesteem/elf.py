7d1e1670a6417e73709ab8a3c1cb11ca02a87f4d
Louis Granboulan <louis.granboulan@eads.net>
2012-11-22 15:25:08 +0100

    CPU types

--- elfesteem/elf.py
+++ elfesteem/elf.py
@@ -118,6 +118,88 @@ ET_HIOS =         0xfeffL         # OS-specific range end
 ET_LOPROC =       0xff00L         # Processor-specific range start
 ET_HIPROC =       0xffffL         # Processor-specific range end
 
+# Legal values for e_machine (architecture).
+
+EM_NONE =         0              # No machine
+EM_M32 =          1              # AT&T WE 32100
+EM_SPARC =        2              # SUN SPARC
+EM_386 =          3              # Intel 80386
+EM_68K =          4              # Motorola m68k family
+EM_88K =          5              # Motorola m88k family
+EM_860 =          7              # Intel 80860
+EM_MIPS =         8              # MIPS R3000 big-endian
+EM_S370 =         9              # IBM System/370
+EM_MIPS_RS3_LE = 10              # MIPS R3000 little-endian
+
+EM_PARISC =      15              # HPPA
+EM_VPP500 =      17              # Fujitsu VPP500
+EM_SPARC32PLUS = 18              # Sun's "v8plus"
+EM_960 =         19              # Intel 80960
+EM_PPC =         20              # PowerPC
+EM_PPC64 =       21              # PowerPC 64-bit
+EM_S390 =        22              # IBM S390
+
+EM_V800 =        36              # NEC V800 series
+EM_FR20 =        37              # Fujitsu FR20
+EM_RH32 =        38              # TRW RH-32
+EM_RCE =         39              # Motorola RCE
+EM_ARM =         40              # ARM
+EM_FAKE_ALPHA =  41              # Digital Alpha
+EM_SH =          42              # Hitachi SH
+EM_SPARCV9 =     43              # SPARC v9 64-bit
+EM_TRICORE =     44              # Siemens Tricore
+EM_ARC =         45              # Argonaut RISC Core
+EM_H8_300 =      46              # Hitachi H8/300
+EM_H8_300H =     47              # Hitachi H8/300H
+EM_H8S =         48              # Hitachi H8S
+EM_H8_500 =      49              # Hitachi H8/500
+EM_IA_64 =       50              # Intel Merced
+EM_MIPS_X =      51              # Stanford MIPS-X
+EM_COLDFIRE =    52              # Motorola Coldfire
+EM_68HC12 =      53              # Motorola M68HC12
+EM_MMA =         54              # Fujitsu MMA Multimedia Accelerator*/
+EM_PCP =         55              # Siemens PCP
+EM_NCPU =        56              # Sony nCPU embeeded RISC
+EM_NDR1 =        57              # Denso NDR1 microprocessor
+EM_STARCORE =    58              # Motorola Start*Core processor
+EM_ME16 =        59              # Toyota ME16 processor
+EM_ST100 =       60              # STMicroelectronic ST100 processor
+EM_TINYJ =       61              # Advanced Logic Corp. Tinyj emb.fam*/
+EM_X86_64 =      62              # AMD x86-64 architecture
+EM_PDSP =        63              # Sony DSP Processor
+
+EM_FX66 =        66              # Siemens FX66 microcontroller
+EM_ST9PLUS =     67              # STMicroelectronics ST9+ 8/16 mc
+EM_ST7 =         68              # STmicroelectronics ST7 8 bit mc
+EM_68HC16 =      69              # Motorola MC68HC16 microcontroller
+EM_68HC11 =      70              # Motorola MC68HC11 microcontroller
+EM_68HC08 =      71              # Motorola MC68HC08 microcontroller
+EM_68HC05 =      72              # Motorola MC68HC05 microcontroller
+EM_SVX =         73              # Silicon Graphics SVx
+EM_ST19 =        74              # STMicroelectronics ST19 8 bit mc
+EM_VAX =         75              # Digital VAX
+EM_CRIS =        76              # Axis Communications 32-bit embedded processor
+EM_JAVELIN =     77              # Infineon Technologies 32-bit embedded processor
+EM_FIREPATH =    78              # Element 14 64-bit DSP Processor
+EM_ZSP =         79              # LSI Logic 16-bit DSP Processor
+EM_MMIX =        80              # Donald Knuth's educational 64-bit processor
+EM_HUANY =       81              # Harvard University machine-independent object files
+EM_PRISM =       82              # SiTera Prism
+EM_AVR =         83              # Atmel AVR 8-bit microcontroller
+EM_FR30 =        84              # Fujitsu FR30
+EM_D10V =        85              # Mitsubishi D10V
+EM_D30V =        86              # Mitsubishi D30V
+EM_V850 =        87              # NEC v850
+EM_M32R =        88              # Mitsubishi M32R
+EM_MN10300 =     89              # Matsushita MN10300
+EM_MN10200 =     90              # Matsushita MN10200
+EM_PJ =          91              # picoJava
+EM_OPENRISC =    92              # OpenRISC 32-bit embedded processor
+EM_ARC_A5 =      93              # ARC Cores Tangent-A5
+EM_XTENSA =      94              # Tensilica Xtensa Architecture
+
+EM_ALPHA =       0x9026
+
 # Legal values for sh_type (section type). 
 
 SHT_NULL =          0             # Section header table entry unused
