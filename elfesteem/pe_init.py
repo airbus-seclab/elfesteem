airbus-seclab/elfesteem

commit d3808e272081079bb34191084edc3b2e68f0dcac

pe: robust to more atypical PE files

elfesteem/pe_init.py
@@ -448,12 +448,14 @@ class PE(object):
         if section is None and self.has_relocatable_sections():
             # TODO: .obj cannot convert rva2off without knowing the section
             return None
-        # Special case rva in header
-        if rva < self.NThdr.sizeofheaders:
-            return rva
         s = self.getsectionbyrva(rva, section)
         if s is None:
-            return None
+            # e.g. Ange Albertini's tinyW7_3264.exe where sizeofheaders is 0
+            # therefore the import table is in no section but not detected as
+            # in the headers.
+            # The test rva < self.NThdr.sizeofheaders from older elfesteem
+            # seems redundant with this one.
+            return rva
         return rva-s.vaddr+s.scn_baseoff
 
     def off2rva(self, off):

elfesteem/pe.py
@@ -1046,7 +1046,7 @@ class SHList(CArray):
 #   DirEnt SECURITY     in no section
 #   DirEnt TLS          in .rdata
 
-from visual_studio_mangling import symbol_demangle
+from elfesteem.visual_studio_mangling import symbol_demangle
 
 class CArrayDirectory(CArray):
     def unpack(self, c, o):
@@ -1080,9 +1080,6 @@ class ImportNamePtr(CStruct):
             self.name = self.obj
         else:
             off = self.parent.parent.rva2off(self.rva)
-            if self.rva < self.parent.parent.parent.parent.NThdr.sizeofheaders:
-                # Negate the hack in rva2off
-                off = None
             # When parsing 'firstthunk', either "off' is None
             # or it is identical to 'originalfirstthunk'.
             # But that's just what is usually the case, a valid PE
@@ -1163,6 +1160,11 @@ class DirImport(CArrayDirectory):
         # that the ImportDescriptor does not need to be all zeroes to be a
         # terminator.
         return elt.name_rva == 0 or elt.firstthunk == 0
+        # According to Ange Albertini's manyimportsW7.exe the AddressOfIndex
+        # field of the TLS directory is a terminator too; but at this point
+        # the TLS directory has not been parsed by elfesteem. This will be
+        # handled if we handle relocations, and parse the file in multiple
+        # passes.
     def add_imports(self, *args):
         # We add a new ImportDescriptor for each new DLL
         # We need to avoid changing RVA of the current IAT, because they

readpe.py
@@ -96,8 +96,9 @@ def print_sections(e):
                 class NoSection(object):
                     name = '<no section>'
                 n = NoSection()
+            dirname = pe.constants['DIRECTORY_ENTRY'].get(i, '<noname>')
             print("%2d %15s %#10x %#10x %12s" %(i,
-                   pe.constants['DIRECTORY_ENTRY'][i],
+                   dirname,
                    s.rva, s.size,
                    '' if s.size == 0 else n.name.strip('\0')
                    ))

commit c4c23f9f7e9868c6f1823563bd8dc4c7f337d7bb

pe: patch d3808e2 introduced a bug

elfesteem/pe_init.py
@@ -448,14 +448,18 @@ class PE(object):
         if section is None and self.has_relocatable_sections():
             # TODO: .obj cannot convert rva2off without knowing the section
             return None
+        # Special case rva in header
+        if rva < self.NThdr.sizeofheaders:
+            return rva
         s = self.getsectionbyrva(rva, section)
         if s is None:
             # e.g. Ange Albertini's tinyW7_3264.exe where sizeofheaders is 0
             # therefore the import table is in no section but not detected as
             # in the headers.
-            # The test rva < self.NThdr.sizeofheaders from older elfesteem
-            # seems redundant with this one.
-            return rva
+            # We use 0x400 because it is the normal size for headers
+            if rva < 0x400:
+                return rva
+            return None
         return rva-s.vaddr+s.scn_baseoff
 
     def off2rva(self, off):