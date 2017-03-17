756f336ecfe96999f1ec4cb55f5186872b23f1f8
Louis Granboulan <Louis.Granboulan@airbus.com>
2017-03-17 13:37:28 +0100

    Add python 3.2 and 3.3 in Travis CI

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ from distutils.core import setup
 setup(
     name = 'ELF-Esteem',
     version = '0.1',    
-    packages=['elfesteem'],
-    scripts = ['examples/readelf.py'],
+    packages = ['elfesteem'],
+    requires = ['python (>= 2.4)'],
+    scripts = ['examples/readelf.py','examples/otool.py','examples/readpe.py'],
     # Metadata
     author = 'Philippe BIONDI',
     author_email = 'phil(at)secdev.org',
     description = 'ELF-Esteem: ELF file manipulation library',
     license = 'LGPLv2.1',
+    url = 'https://github.com/airbus-seclab/elfesteem',
     # keywords = '',
-    # url = '',
 )
