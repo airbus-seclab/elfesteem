19eec7a50a284cb0a8dfce0481f6081d0332394b
Louis Granboulan <Louis.Granboulan@airbus.com>
2017-05-29 15:11:51 +0200

    strpatchwork: avoid deprecated tostring() in python3

--- elfesteem/strpatchwork.py
+++ elfesteem/strpatchwork.py
@@ -14,10 +14,13 @@ class StrPatchwork(object):
         self.s_cache = s
         self.paddingbyte=paddingbyte
     def __str__(self):
-        return self.s.tostring() # Needed for miasm2 :-(
+        return self.pack() # Needed for miasm2 :-(
         raise AttributeError("Use pack() instead of str()")
     def pack(self):
-        return self.s.tostring()
+        if sys.version_info[0] >= 3:
+            return self.s.tobytes()
+        else:
+            return self.s.tostring()
 
     def __getitem__(self, item):
         s = self.s
@@ -35,12 +38,15 @@ class StrPatchwork(object):
                     start = item.start
                     if start is None: start = 0
                     r = array("B",self.paddingbyte*(end-start))
-            return r.tostring()
         else:
             if item > len(s):
                 return self.paddingbyte
             else:
-                return array("B",[s[item]]).tostring()
+                r = array("B",[s[item]])
+        if sys.version_info[0] >= 3:
+            return r.tobytes()
+        else:
+            return r.tostring()
     def __setitem__(self, item, val):
         if val == None:
             return
@@ -58,7 +64,7 @@ class StrPatchwork(object):
 
 
     def __repr__(self):
-        return "<Patchwork %r>" % self.s.tostring()
+        return "<Patchwork %r>" % self.pack()
     def __len__(self):
         return len(self.s)
     def __contains__(self, val):
@@ -69,11 +75,11 @@ class StrPatchwork(object):
 
     def find(self, pattern, *args):
         if not self.s_cache:
-            self.s_cache = self.s.tostring()
+            self.s_cache = self.pack()
         return self.s_cache.find(pattern, *args)
 
     def rfind(self, pattern, *args):
         if not self.s_cache:
-            self.s_cache = self.s.tostring()
+            self.s_cache = self.pack()
         return self.s_cache.rfind(pattern, *args)
 
